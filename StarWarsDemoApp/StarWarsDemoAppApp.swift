b77d8269a5bbc933a7d64083d9989334a5bd6a64
Yannick Rietz <rietz@projektionisten.de>
2023-12-10 10:24:52 +0100

    Initial Commit

---
+++ StarWarsDemoApp/StarWarsDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsDemoAppApp.swift
+//  StarWarsDemoApp
+//
+//  Created by Yannick Rietz on 10.12.23.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
